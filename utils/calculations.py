41d6d6f7a9f063f72287647c52e16d49c5b2e2f6
Suman Sourabh <sumansourabhcse@gmail.com>
2026-04-11 07:11:12 +0200

    Update calculations.py

--- utils/calculations.py
+++ utils/calculations.py
@@ -1,17 +1,23 @@
 import pandas as pd
+import os
 
-def load_data(file_path="data/investments.csv"):
-    df = pd.read_csv(file_path, parse_dates=["Date"], dayfirst=True)
-    return df
+def load_all_funds(base_path="mutualfund"):
+    all_data = []
+    for fund in os.listdir(base_path):
+        file_path = os.path.join(base_path, fund, "fund.csv")
+        if os.path.exists(file_path):
+            df = pd.read_csv(file_path, parse_dates=["Date"], dayfirst=True)
+            df["FundName"] = fund
+            all_data.append(df)
+    return pd.concat(all_data, ignore_index=True)
 
 def overall_summary(df):
     total_invested = df["Amount"].sum()
     current_value = (df["Units"] * df["NAV"]).sum()
-    profit_loss = current_value - total_invested
     return {
         "Total Invested": total_invested,
         "Current Value": current_value,
-        "Profit/Loss": profit_loss
+        "Profit/Loss": current_value - total_invested
     }
 
 def fund_summary(df, fund_name):
